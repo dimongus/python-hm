f96407b07de0a379fd06f0d01ed034a3c22a2eca
dmitrii <dima1994.dl@gmail.com>
2023-03-11 10:39:45 +0900

    try 432114

--- 18.py
+++ 18.py
@@ -1,7 +1,16 @@
-a = [int(i) for i in input('Введите элементы через пробел ').split()]
-b = int(input('Введите число: '))
-number = 0
-for i in range(len(a)):
-     if b - a[i] < b - number and b - a[i] < 0:
-        number = i
-print(f"Ближайшее число: {a[number]}")
+n = int(input('Введите количество элементов: '))
+count = 0
+lst = list(map(int, input('Введите элементы списка через пробел: ').split()))
+
+if len(lst) != n or n == 0:
+    print('Число введенных элементов не соответствуют заявленному количеству!')
+else:
+    X = int(input('Введите число X: '))
+    mini = abs(X - lst[0])
+    index = 0
+    for i in range(n):
+        count = abs(X - lst[i])
+        if count < mini:
+            mini = count
+            index = i
+    print(f'Число {lst[index]} - ближайшее к числу {X}')
